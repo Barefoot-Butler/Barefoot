7817bf2da0a678d3104a02296bd52da5d89e9dcb
Gabriel Wong <gabrieljw1@gmail.com>
2018-06-11 17:59:22 -0400

    Initial Commit

---
+++ Barefoot/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Barefoot
+//
+//  Created by Gabriel Wong on 6/11/18.
+//  Copyright © 2018 Gabriel Wong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Barefoot/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Barefoot
+//
+//  Created by Gabriel Wong on 6/11/18.
+//  Copyright © 2018 Gabriel Wong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
